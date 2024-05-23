86e118ea7767ca0a452062a4e38106ca37d56ee3
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-05-23 18:00:00 +0900

    add: add font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,36 +1,35 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: { Pretendard : ['Pretendard']},
     extend: {
       colors: {
-        'white': '#ffffff',
-        'black': '#000000',
-        'navy': '#072AC8',
-        'blue': '#1E96FC',
-        'lightblue': '#E9F5FF',
-        'mint': '#46E5DC',
-        'yellow': '#FFF16C',
-        'darkgray': '#5B5C5E',
-        'gray': '#8C8C8C',
+        white: '#ffffff',
+        black: '#000000',
+        navy: '#072AC8',
+        blue: '#1E96FC',
+        lightblue: '#E9F5FF',
+        mint: '#46E5DC',
+        yellow: '#FFF16C',
+        darkgray: '#5B5C5E',
+        gray: '#8C8C8C',
       },
-      fontFamily: {},
       borderRadius: {
         '8': '8px',
-        '20': '20px'
+        '20': '20px',
       },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
