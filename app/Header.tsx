c9ae3319e9dd59ae58043f9044531c02a6427168
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-05-24 15:54:19 +0900

    chore: edit header style

--- app/Header.tsx
+++ app/Header.tsx
@@ -11,27 +11,27 @@ export default function Header() {
 
   const renderSearchBar = () => {
     return (
-      <div className="ml-20 rounded-16 flex h-[44px] w-[600px] items-center justify-between bg-lightblue">
+      <div className="rounded-16 ml-20 flex h-[44px] w-[600px] items-center justify-between bg-lightblue">
         <div className="ml-5 flex items-center">
-          <img src="/header_search.svg" className="h-[24px] w-[24px]" />
+          <img src="/header_search.svg" className="h-[20px] w-[20px]" />
           <input
             type="text"
             placeholder="검색어를 입력하세요"
-            className="ml-3.5 bg-transparent text-[14px] outline-0"
+            className="ml-3.5 bg-transparent text-sm outline-0"
           />
         </div>
-        <img src="/header_filter.svg" className="mr-6 h-[24px] w-[24px] cursor-pointer" />
+        <img src="/header_filter.svg" className="mr-6 h-[20px] w-[20px] cursor-pointer" />
       </div>
     )
   }
 
   const renderLoginSection = () => {
     return (
-      <div className="flex justify-end w-72">
-        <button className="mr-4 rounded-16 h-[44px] w-[88px] border-[1px] border-blue text-blue">
+      <div className="flex w-72 justify-end">
+        <button className="rounded-16 mr-4 h-[44px] w-[84px] border-[1px] border-blue text-sm text-blue">
           로그인
         </button>
-        <button className="rounded-16 h-[44px] w-[88px] bg-blue text-blue text-white">
+        <button className="rounded-16 h-[44px] w-[84px] bg-blue text-sm text-blue text-white">
           Sign Up
         </button>
       </div>
