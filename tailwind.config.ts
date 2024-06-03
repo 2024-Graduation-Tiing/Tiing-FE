3346b510ab5855af14df81d7d13c90244218b070
3eonah <3eonah.lee@gmail.com>
2024-06-03 17:44:43 +0900

    add: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,7 +28,11 @@ const config: Config = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-diagonal':' linear-gradient(130deg, var(--tw-gradient-stops))'
       },
+      width: {
+        '150':'150%'
+      }
     },
   },
   plugins: [
