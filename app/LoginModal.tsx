ff2207fb17a6af66a4c8406083781d6658ba4acb
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-07-25 18:03:56 +0900

    chore: 모달 배경 클릭 시 창 닫기

--- app/LoginModal.tsx
+++ app/LoginModal.tsx
@@ -6,7 +6,12 @@ const LoginModal = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter()
 
   return (
-    <div className="fixed left-0 top-0 z-50 box-border flex h-screen w-screen justify-center overflow-hidden bg-black bg-opacity-50">
+    <div
+      className="fixed left-0 top-0 z-50 box-border flex h-screen w-screen justify-center overflow-hidden bg-black bg-opacity-50"
+      onClick={() => {
+        router.back()
+      }}
+    >
       <div className="absolute top-[120px] w-[500px] rounded-16 bg-white px-[80px] py-[50px]">
         <img
           src="/modal_close_btn.svg"
