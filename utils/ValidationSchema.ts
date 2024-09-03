d758ef52737a114e380b57b376adb19fe41d74b9
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-09-04 00:49:16 +0900

    add: sign up validation schema using yup

--- utils/ValidationSchema.ts
+++ utils/ValidationSchema.ts
@@ -14,7 +14,10 @@ export const SignUpValidationSchema = yup.object().shape({
     .required('필수 입력 항목입니다.')
     .min(8, '8-16자로 입력해주세요.')
     .max(16, '8-16자로 입력해주세요.')
-    .matches(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[\W_]).+$/, '영문, 숫자, 특수문자를 모두 포함하여 입력해주세요.'),
+    .matches(
+      /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[\W_]).+$/,
+      '영문, 숫자, 특수문자를 모두 포함하여 입력해주세요.',
+    ),
   passwordCheck: yup
     .string()
     .oneOf([yup.ref('password')], '비밀번호가 일치하지 않습니다.')
