e4323ef733e9f584e787cb7c08f7ce8ff77ed2dc
Seonah Lee <3eonah.lee@gmail.com>
2024-09-15 23:39:09 +0900

    chore: 이미지 전송 추가중

--- app/chat/[roomId]/Bubble.tsx
+++ app/chat/[roomId]/Bubble.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 //
@@ -15,19 +16,33 @@ type BubbleProps = {
   isMine: boolean
 }
 
+//
+//
+//
+
+const isImageUrl = (url: string) => {
+  return url.match(/\.(jpeg|jpg|gif|png|webp)$/i) !== null
+}
+
 const Bubble = ({ message, isMine }: BubbleProps) => {
+  const isImage = isImageUrl(message.message)
+
   if (isMine) {
     return (
-      <div className="mb-3 flex flex-col justify-end">
-        <div className="mb-1	w-fit text-pretty text-pretty rounded-b-xl rounded-l-xl bg-blue p-3 text-sm text-white">
-          {message.message}
+      <div className="mb-3 flex flex-col items-end">
+        <div className="mb-1 w-fit text-pretty text-pretty rounded-b-xl rounded-l-xl bg-blue p-3 text-sm text-white">
+          {isImage ? (
+            <Image src={message.message} width={300} height={300} alt="sent image" />
+          ) : (
+            message.message
+          )}
         </div>
-        <div className="self-end text-xs text-gray-500">{message.sendingTime}</div>
+        <div className="text-xs text-gray-500">{message.sendingTime}</div>
       </div>
     )
   } else {
     return (
-      <div className="mb-3 flex flex-col justify-start">
+      <div className="mb-3 flex flex-col">
         <div className="mb-1 w-fit rounded-b-xl rounded-r-xl bg-skyblue p-3 text-sm text-darkgray">
           {message.message}
         </div>
