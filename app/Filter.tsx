5fa3cff14f7e44dbdc7410d8aacbca3976f232bc
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-09-16 01:11:14 +0900

    feat: handle click event using search params in filter

--- app/Filter.tsx
+++ app/Filter.tsx
@@ -1,19 +1,44 @@
 'use client';
 
 import { FILTERS } from '@/app/lib/filters';
+import { useSearchParams, useRouter } from 'next/navigation';
 
 //
 //
 //
 
 export default function Filter() {
+  const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams?.toString());
+  const router = useRouter();
+
+  /**
+   * 
+   */
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const clickedItem = e.currentTarget.textContent;
+    const clickedCategory = e.currentTarget.parentElement?.firstChild?.textContent;
+
+    if (clickedItem && clickedCategory) {
+      if (searchParams?.getAll(clickedCategory).includes(clickedItem)) {
+        const currentValues = params.getAll(clickedCategory);
+        const updatedValues = currentValues.filter((value) => value !== clickedItem);
+        params.delete(clickedCategory);
+        updatedValues.forEach((value) => params.append(clickedCategory, value));
+      } else {
+        params.append(clickedCategory, clickedItem);
+      }
+      router.replace(`?${params.toString()}`);
+    }
+  }
+
   return (
     <div className="w-[1200px] rounded-16 bg-lightblue px-10 py-6">
       {Object.entries(FILTERS).map(([key, value]) => (
         <div className="mb-3 flex items-center" key={key}>
           <h3 className="text-md mr-10">{value.title}</h3>
           {value.options.map((option) => (
-            <button className="select-btn-default mr-4 border-0">
+            <button className="select-btn-default mr-4 border-0" key={option.id} onClick={handleClick}>
               {option.name}
             </button>
           ))}
