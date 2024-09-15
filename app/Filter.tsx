48eb1cdd3f50807b96f6dee80e33bef2f0191f1f
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-09-16 02:10:11 +0900

    chore: add style on clicked items

--- app/Filter.tsx
+++ app/Filter.tsx
@@ -13,16 +13,19 @@ export default function Filter() {
   const router = useRouter();
 
   /**
-   * 
+   *
    */
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     const clickedItem = e.currentTarget.textContent;
-    const clickedCategory = e.currentTarget.parentElement?.firstChild?.textContent;
+    const clickedCategory =
+      e.currentTarget.parentElement?.firstChild?.textContent;
 
     if (clickedItem && clickedCategory) {
       if (searchParams?.getAll(clickedCategory).includes(clickedItem)) {
         const currentValues = params.getAll(clickedCategory);
-        const updatedValues = currentValues.filter((value) => value !== clickedItem);
+        const updatedValues = currentValues.filter(
+          (value) => value !== clickedItem,
+        );
         params.delete(clickedCategory);
         updatedValues.forEach((value) => params.append(clickedCategory, value));
       } else {
@@ -30,7 +33,7 @@ export default function Filter() {
       }
       router.replace(`?${params.toString()}`);
     }
-  }
+  };
 
   return (
     <div className="w-[1200px] rounded-16 bg-lightblue px-10 py-6">
@@ -38,7 +41,11 @@ export default function Filter() {
         <div className="mb-3 flex items-center" key={key}>
           <h3 className="text-md mr-10">{value.title}</h3>
           {value.options.map((option) => (
-            <button className="select-btn-default mr-4 border-0" key={option.id} onClick={handleClick}>
+            <button
+              className={`${searchParams?.getAll(value.title).includes(option.name) ? 'select-btn-selected' : 'select-btn-default border-0'} mr-4`}
+              key={option.id}
+              onClick={handleClick}
+            >
               {option.name}
             </button>
           ))}
