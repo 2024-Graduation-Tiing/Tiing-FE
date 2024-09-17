aabec9dbb7cf4f501727419011d00f3ea9e77032
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-09-17 16:40:57 +0900

    chore: change env name

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -6,12 +6,12 @@ import { getCookie } from 'cookies-next';
 //
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_SPRING_URL,
   withCredentials: true,
 })
 
 export const authApi = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_SPRING_URL,
   headers: {
     'Authorization': `Bearer ${getCookie('accessToken')}`,
     withCredentials: true,
