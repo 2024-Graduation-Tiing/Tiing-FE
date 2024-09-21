9ac132a7f42103e5d73ed235ea7fcc69abcc8483
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-09-21 15:42:06 +0900

    chore: define profile and proposal data type

--- typings/type.ts
+++ typings/type.ts
@@ -1,4 +1,34 @@
-export default interface GetApiUserDetail {
+export interface GetApiUserDetail {
   role: string;
   memberId: string;
+}
+
+type DynamicObject = { [key: string]: string };
+
+export interface ProfileInfo {
+  entertainer_id: string;
+  name: string;
+  plaforms: DynamicObject;
+  age: number;
+  height: number;
+  weight: number;
+  keywords: DynamicObject;
+  description: string;
+  images: DynamicObject;
+  career?: DynamicObject;
+  entertainer: { gender: number | string };
+}
+
+export interface ProposalInfo {
+  id: number;
+  scouter_id: string;
+  company: string;
+  title: string;
+  platforms: DynamicObject;
+  gender_condition: DynamicObject;
+  keywords: DynamicObject;
+  description: string;
+  image: string;
+  end_date: Date;
+  age_condition: DynamicObject;
 }
