9e5cc4ba521d03bf4c98cf7d1b999657a43e776e
Seonah Lee <3eonah.lee@gmail.com>
2024-09-24 03:45:11 +0900

    chore: retrun값 변경

--- app/api/matches/route.ts
+++ app/api/matches/route.ts
@@ -1,4 +1,5 @@
 import { db } from '@/app/lib/db';
+import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
   const { userId, userRole } = await request.json();
@@ -24,34 +25,34 @@ export async function GET(request: Request) {
     console.error(err);
   }
 
-  return matches;
+  return new NextResponse(JSON.stringify(matches), { status: 200 });
 }
 
-export async function POST(request: Request) {
-  const { entertainerId, proposalId } = await request.json();
+// export async function POST(request: Request) {
+//   const { entertainerId, proposalId } = await request.json();
 
-  try {
-    // 제안서 조회
-    const proposal = await db.proposal.findUnique({
-      where: {
-        id: proposalId,
-      },
-    });
-    if (!proposal) {
-      return new Response('Proposal not found', { status: 404 });
-    }
+//   try {
+//     // 제안서 조회
+//     const proposal = await db.proposal.findUnique({
+//       where: {
+//         id: proposalId,
+//       },
+//     });
+//     if (!proposal) {
+//       return new Response('Proposal not found', { status: 404 });
+//     }
 
-    // 새로운 match 데이터 추가
-    const newMatch = await db.matches.create({
-      data: {
-        entertainer_id: entertainerId,
-        proposal_id: proposalId,
-        matched: false,
-      },
-    });
-    return new Response(JSON.stringify(newMatch), { status: 201 });
-  } catch (err) {
-    console.error(err);
-    return new Response('Error creating match', { status: 500 });
-  }
-}
+//     // 새로운 match 데이터 추가
+//     const newMatch = await db.matches.create({
+//       data: {
+//         entertainer_id: entertainerId,
+//         proposal_id: proposalId,
+//         matched: false,
+//       },
+//     });
+//     return new Response(JSON.stringify(newMatch), { status: 201 });
+//   } catch (err) {
+//     console.error(err);
+//     return new Response('Error creating match', { status: 500 });
+//   }
+// }
