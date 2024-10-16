9252ec38d44d5779ed5ec1eb0ef44a46cc70e770
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-10-16 17:42:05 +0900

    chore: dynamic import for components with searchParams

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import { Suspense } from 'react';
 import Profiles from './Profiles';
 import ScrollBtn from './ScrollBtn';
+import dynamic from 'next/dynamic';
+
+//
+//
+//
+
+const ProfilesClient = dynamic(() => import('./Profiles'), { ssr: false });
 
 //
 //
@@ -10,7 +17,7 @@ export default function Home() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <div className="flex flex-col items-center">
-        <Profiles />
+        <ProfilesClient />
         <ScrollBtn />
       </div>
     </Suspense>
